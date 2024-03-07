627d3a1d08a3bb778b8a8d389e15131247393a21
Endand <endand2898@gmail.com>
2024-03-07 00:25:49 -0500

    Now if your hover over the squares, it changes to a random color.

--- sketch.js
+++ sketch.js
@@ -2,12 +2,28 @@ const body=document.querySelector('body');
 const container= document.querySelector('.container');
 
 
-for(let i=0;i<16;i++){
-   for(let j=0;j<16;j++){
+for(let i=0;i<16*16;i++){
       const square = document.createElement('div');
       square.classList.add('square');
       container.appendChild(square);
-   }
 }
 
 body.appendChild(container);
+
+function changeColor(square){
+   square.style.backgroundColor='#' + Math.floor(Math.random()*16777215).toString(16);
+}
+
+const squares= document.querySelectorAll('.square');
+squares.forEach(square=>{
+   square.addEventListener("mouseover",()=>{
+      changeColor(square);});
+})
+
+let number=16;
+const form= document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+   e.preventDefault();
+   number= document.querySelector(".number").value;
+});
+
