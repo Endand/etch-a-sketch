0112fb0726d16f22975962ba13e1ead356973e18
Endand <endand2898@gmail.com>
2024-03-06 23:02:15 -0500

    Added Boxes using CSS
    Now the squares are visible on the webpage

---
+++ sketch.js
@@ -0,0 +1,13 @@
+const body=document.querySelector('body');
+const container= document.querySelector('.container');
+
+
+for(let i=0;i<16;i++){
+   for(let j=0;j<16;j++){
+      const square = document.createElement('div');
+      square.classList.add('square');
+      container.appendChild(square);
+   }
+}
+
+body.appendChild(container);
